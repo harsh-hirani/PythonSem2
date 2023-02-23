bd39899d61c801a1609c6e45d674fd05f035420a
Harsh Hirani <hiraniharsh7@gmail.com>
2023-02-23 11:37:44 +0530

    extra

--- lab.py
+++ lab.py
@@ -1,7 +1,7 @@
 # Given three points (x1,y1), (x2,y2) and (x3,y3), check if all the three points fall on one straight line.
-f = [1,1]
-s= [2,2]
-t = [3,4]
+f = [int(input('enter x1 :')),int(input('enter y1 :'))]
+s = [int(input('enter x2 :')),int(input('enter y1 :'))]
+t = [int(input('enter x3 :')),int(input('enter y1 :'))]
 
 if (f[0]==s[0] and s[0]==t[0]) or (f[1]==s[1] and s[1]==t[1]):
     print("in lines")
