77823a268142f202cd5580f4f7925460a21f9b32
Harsh Hirani <hiraniharsh7@gmail.com>
2023-02-23 13:56:38 +0530

    p3 6

--- lab.py
+++ lab.py
@@ -1,25 +1,7 @@
-# WAP to ask the number of lines from the user and print the following pattern:
-#  1
-#  AB
-#  234
-#  ABCD 
-import string
-alphabet = list(string.ascii_uppercase)
-lines = int(input('enter the lines'))
-if lines < 1:
-    print('lines should be more than 0.')
-    exit()
-else:
-    for i in range(1,lines+1):
-        print()
-        for k in range(0,lines-i):
-            print('  ',end="")
-        if( i%2 == 0 ):
-            #chars
-            for j in range(0,i):
-                print(alphabet[j],end=" ")
-            
-        else:
-            # numbers
-            for j in range(1,i+1):
-                print(j,end=" ")
+# Display the user specified term in Fibonacci series using a recursive function fibo()
+def fibo(n):
+    if n<2:
+        return 1
+    return (fibo(n-1)+fibo(n-2))
+value = int(input('Enter the term of Fibonacci series: '))
+print('nth term of Fibonacci series: ',fibo(value))
