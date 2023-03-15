6d5f1861e27d00e6561a2e580168ddcc3fa6781d
Harsh Hirani <hiraniharsh7@gmail.com>
2023-03-15 20:38:53 +0530

    p4 4

--- lab.py
+++ lab.py
@@ -1,8 +1,20 @@
-# p4 3 - WAP program to print a specified list after removing the 0th, 2nd, 4th and 5th elements
-test_list = [0,1,3,4,5,6,7,8,9]
-test_list = [e for count,e in enumerate(test_list) if count not in [0,2,4,5] ]
-# test_list.pop(5)
-# test_list.pop(4)
-# test_list.pop(2)
-# test_list.pop(0)
-print(test_list)
+# p4 4 -Formulate a problem definition of your own and demonstrate functions as objects.
+def sum(props):
+    sum = 0
+    for s in props:
+        sum += s
+    return sum
+def fact(n):
+    if n<2:
+        return 1
+    return n*fact(n-1)
+def QnR(n, d):
+    return {'quotient':int(n/d),'reminder':n%d}
+function_dict = {
+    "sum":sum,
+    "fact":fact,
+    "qnr":QnR
+}
+print(type(function_dict["sum"]),function_dict["sum"]([1,2,3,4,5,6,7,8,9]))
+print(type(function_dict["fact"]),function_dict["fact"](5))
+print(type(function_dict["qnr"]),function_dict["qnr"](5,2))
