031c67b3de813c4b8cea8ef26fb8b811a98fb70c
Harsh Hirani <hiraniharsh7@gmail.com>
2023-03-15 21:39:22 +0530

    p5 4

--- lab.py
+++ lab.py
@@ -1,13 +1,31 @@
-# p4 6 - WAP to check whether an input sentence is a pangram or not. 
-import string
-alphabets = list(string.ascii_lowercase)
-used_alphabets = set()
-str = input('Enter a sting to check if it is pangram or not: ')
-
-for char in str.lower():
-    if char in alphabets:
-        used_alphabets.add(char)
-if len(used_alphabets) == 26:
-    print("yes")
-else:
-    print('no')
+# p5 4 - WAP to implement matrix multiplication
+mat1 = list()
+mat2 = list()
+raw = int(input('Raw: '))
+column = int(input('column: '))
+# function to take matrix as input
+def matcollect(r,c,typ=''):
+    returnlist = list()
+    for i in range(0,r):
+        tempList = list()
+        for j in range(0,c):
+            if typ == 'nor':
+                tempList.append(0)
+            else:
+                tempList.append(int(input(f"{i+1}x{j+1} : ")))
+                
+        returnlist.append(tempList)
+    return returnlist
+print('first matrix:')
+mat1 = matcollect(raw,column)
+print('second matrix:')
+mat2 = matcollect(raw,column)
+mat3 = matcollect(raw,column,typ='nor')
+# multiplying
+for i,f in enumerate(mat1):
+    for j,iner in enumerate(f):
+        for c,asiehi in enumerate(f):
+            mat3[i][j] += mat1[i][c]*mat2[c][j]
+print('output')
+for q in mat3:
+    print(q)
