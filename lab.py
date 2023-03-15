4900973c02f7233c6cd8c6b9a8e303ec10e5f1cd
Harsh Hirani <hiraniharsh7@gmail.com>
2023-03-15 20:08:21 +0530

    extra p4 1

--- lab.py
+++ lab.py
@@ -1,4 +1,4 @@
 # p4 1 -WAP with function QnR(n,d) that returns quotient and reminder after dividing n by d.
 def QnR(n, d):
-    return [int(n/d),n%d]
-print(QnR(7,4))
+    return {'quotient':int(n/d),'reminder':n%d}
+print(QnR(int(input('Numerator: ')),int(input('Denominator: '))))
