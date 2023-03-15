8d91d5fabd46bb09314ff8657b6b7823c0446c2e
Harsh Hirani <hiraniharsh7@gmail.com>
2023-03-15 20:19:11 +0530

    P4 2

--- lab.py
+++ lab.py
@@ -1,4 +1,14 @@
-# p4 1 -WAP with function QnR(n,d) that returns quotient and reminder after dividing n by d.
-def QnR(n, d):
-    return {'quotient':int(n/d),'reminder':n%d}
-print(QnR(int(input('Numerator: ')),int(input('Denominator: '))))
+# p4 2 - WAP that takes two lists and returns True if they have at least one common member.
+l1_size = int(input('size of First list: '))
+l1 = []
+for i in range(0,l1_size):
+    l1.append(input(f"member{i}:"))
+l2_size = int(input("---------------\nsize of Second list: "))
+l2 = []
+for i in range(0,l2_size):
+    l2.append(input(f"member{i}:"))
+for e in l1:
+    if e in l2:
+        print(f"yes it has '{e}' common in both")
+        exit(0)
+print('not at least one member is common')
