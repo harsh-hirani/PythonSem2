ac7ea98cfd2678376691ebf22a2e566ab36fe7ff
Harsh Hirani <hiraniharsh7@gmail.com>
2023-03-24 23:22:40 +0530

    p5 6

--- lab.py
+++ lab.py
@@ -1,14 +1,18 @@
-# p5 2 - Write a Python script to concatenate following dictionaries to create a new one
-dic1={1:10, 2:20}
-dic2={3:30, 4:40}
-dic3={5:50,6:60}
-
-sumdic=dict()
-
-for i in dic1.keys():
-    sumdic[i]=dic1[i]
-for i in dic2.keys():
-    sumdic[i]=dic2[i]
-for i in dic3.keys():
-    sumdic[i]=dic3[i]
-print(sumdic)
+# p5 6 - comment a code
+# function to check the sequence in given string in order of pattern
+# given code can be written in a different way like this:
+from collections import OrderedDict
+def checkOrder(input, pattern):
+    # dict = OrderedDict.fromkeys(input)
+    ptrlen = 0 # later to evalute how many pattern aplphabet are in order
+    # for key,value in dict.items():
+    for key in input:
+        if (key == pattern[ptrlen]):
+            ptrlen = ptrlen + 1 # if we have a pattern alphabet in string then by adding it by one we go to the next pattern alphabet
+        if (ptrlen == (len(pattern))):# if we have a pattern means we have all the pattern alphabet in string in order like pattern
+            return 'true'
+    return 'false'
+if __name__ == "__main__":
+    input = 'engineers rock'
+    pattern = 'er'
+    print (checkOrder(input,pattern)) 
