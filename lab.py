78757d0992cf510c3c74ef83d63d53e3c1508c77
Harsh Hirani <hiraniharsh7@gmail.com>
2023-03-25 00:01:47 +0530

    p5 6

--- lab.py
+++ lab.py
@@ -1,18 +1,11 @@
-# p5 6 - comment a code
-# function to check the sequence in given string in order of pattern
-# given code can be written in a different way like this:
-from collections import OrderedDict
-def checkOrder(input, pattern):
-    # dict = OrderedDict.fromkeys(input)
-    ptrlen = 0 # later to evalute how many pattern aplphabet are in order
-    # for key,value in dict.items():
-    for key in input:
-        if (key == pattern[ptrlen]):
-            ptrlen = ptrlen + 1 # if we have a pattern alphabet in string then by adding it by one we go to the next pattern alphabet
-        if (ptrlen == (len(pattern))):# if we have a pattern means we have all the pattern alphabet in string in order like pattern
-            return 'true'
-    return 'false'
-if __name__ == "__main__":
-    input = 'engineers rock'
-    pattern = 'er'
-    print (checkOrder(input,pattern)) 
+# p5 6 - Download a .csv file and WAP that extracts the columns indicating name, date, time, shares and price
+import csv
+data = list()
+with open("d.csv") as f:
+    reader = csv.reader(f)
+    headers= next(reader)
+    for i,row in enumerate(reader):
+        if i == 11:
+            break
+        data.append(dict([('date',row[0]),("AAPL",dict([('Open',row[1]),('High',row[2]),('Low',row[3]),('Close',row[4]),('Volume',row[5]),('Adjusted',row[6])])),('more',dict([('dn',row[7]),('mavg',row[8]),('up',row[9]),('dicrection',row[10])]))]))
+print(data)
