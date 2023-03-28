3a39d76984faab4c18e880a2815254994794faf3
Harsh Hirani <hiraniharsh7@gmail.com>
2023-03-28 17:23:26 +0530

    p6 1

--- lab.py
+++ lab.py
@@ -1,11 +1,10 @@
-# p5 6 - Download a .csv file and WAP that extracts the columns indicating name, date, time, shares and price
-import csv
-data = list()
-with open("d.csv") as f:
-    reader = csv.reader(f)
-    headers= next(reader)
-    for i,row in enumerate(reader):
-        if i == 11:
-            break
-        data.append(dict([('date',row[0]),("AAPL",dict([('Open',row[1]),('High',row[2]),('Low',row[3]),('Close',row[4]),('Volume',row[5]),('Adjusted',row[6])])),('more',dict([('dn',row[7]),('mavg',row[8]),('up',row[9]),('dicrection',row[10])]))]))
-print(data)
+# p6 (error code) - 1
+# initialize the amount variable
+amount = 10000
+
+# check that You are eligible to
+# purchase Dsa Self Paced or not
+if(amount>2999)
+	print("You are eligible to purchase Dsa Self Paced")
+# output - SyntaxError: invalid syntax
+#ans - forgot to put : after if statement 
