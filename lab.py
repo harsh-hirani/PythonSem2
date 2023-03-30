d61beffb6b00953724f3bd9920adcd1d6e633333
Harsh Hirani <hiraniharsh7@gmail.com>
2023-03-30 16:43:40 +0530

    p6 10

--- lab.py
+++ lab.py
@@ -1,7 +1,7 @@
-# p6 (error code) - 11
+# p6 (error code) - 10
 # initialize the amount variable
-x='hello'
-if not type(x) is int:
-    raise TypeError('Only integers are allowed')
+x=-1
+if x<0:
+    raise Exception('Sorry, no numbers below zero')
 # output - TypeError: Only integers are allowed
-#ans - String is stored in x but condition check for it to be int and if not raise a type error with msg
+#ans - Since x is less than 0 it will raise exception with msg given
