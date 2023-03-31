ec58ff931b032e004ff4a8dab3929ebe3c9f06ad
Harsh Hirani <hiraniharsh7@gmail.com>
2023-03-31 22:21:25 +0530

    p6 14

--- lab.py
+++ lab.py
@@ -1,11 +1,11 @@
-# p6 (error code) - 13 - 7
+# p6 (error code) - 14 - 7
 # initialize the amount variable
 try:
-  print("Hello")
+  print(x)
 except:
   print("Something went wrong")
-else:
-  print("Nothing went wrong")
+finally:
+  print("The 'try except' is finished")
 
-# output - Print statement didn’t give any error that’s why else statement ran
-#ans - Hello\nNothing went wrong
+# ans -X is not declared so it will give NameError and resolve it in except block
+# #out -Something went wrong \nThe 'try except' is finished
