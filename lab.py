d501883fe1f642b6bedb0d6ff3208d1e142292e5
Harsh Hirani <hiraniharsh7@gmail.com>
2023-03-31 22:32:20 +0530

    p6 16 A

--- lab.py
+++ lab.py
@@ -1,15 +1,6 @@
-# p6 (error code) - 15 - 9
+# p6 (error code) - 16 A
 # initialize the amount variable
-try:
-  f = open("demofile.txt")
-  try:
-    f.write("Lorum Ipsum")
-  except:
-    print("Something went wrong when writing to the file")
-  finally:
-    f.close()
-except:
-  print("Something went wrong when opening the file")
+import re
 
 # ans -File doesn’t exist 
 #out -Something went wrong when opening the file
