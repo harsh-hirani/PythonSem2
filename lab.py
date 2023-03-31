d7e3e595df438b1448b6ab2261b54ed6d6fd7336
Harsh Hirani <hiraniharsh7@gmail.com>
2023-03-31 22:48:54 +0530

    p6 17 A

--- lab.py
+++ lab.py
@@ -1,6 +1,6 @@
 # p6 (error code) - 16 D
 # initialize the amount variable
-#ZeroDevisionError
-y = 5 / 0
+#TypeError: 
 
-#out - ZeroDivisionError: division by zero 
+z = 5 + '402.5'
+#out - TypeError: unsupported operand type(s) for +: 'int' and 'str'
