39e2e62158f9c9b1e50e8d8cce77542ee7793fbc
Harsh Hirani <hiraniharsh7@gmail.com>
2023-04-08 16:07:56 +0530

    p6 17B

--- lab.py
+++ lab.py
@@ -1,6 +1,10 @@
-# p6 (error code) - 16 D
+# p6 (error code) - 17 B
 # initialize the amount variable
-#TypeError: 
+#OSError
+import os
+try :
+    os.mkdir("new_dictionary")
+except OSError as error:
+    print("Error : ",error)
 
-z = 5 + '402.5'
-#out - TypeError: unsupported operand type(s) for +: 'int' and 'str'
+#out - Error : [WinError 183] Cannot create a file when that file already exists: 'new_dictionary'
