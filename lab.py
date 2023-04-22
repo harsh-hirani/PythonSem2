fb140fbba4920e30817491715b3acd102ee01314
Harsh Hirani <hiraniharsh7@gmail.com>
2023-04-22 14:16:38 +0530

    sa3 x1

--- lab.py
+++ lab.py
@@ -1,10 +1,19 @@
-# p6 (error code) - 17 B
-# initialize the amount variable
-#OSError
-import os
-try :
-    os.mkdir("new_dictionary")
-except OSError as error:
-    print("Error : ",error)
+# sa3 x1
 
-#out - Error : [WinError 183] Cannot create a file when that file already exists: 'new_dictionary'
+import numpy as np
+arr1 = np.array([1, 2, 3])
+arr2 = np.array([4, 5, 6])
+arr = np.concatenate((arr1, arr2))#
+print(arr)
+
+ar1 = np.array([[1, 2], [3, 4]])
+ar2 = np.array([[5, 6], [7, 8]])
+ar = np.concatenate((ar1, ar2), axis=1) 
+print(ar)#concatenate these two arrays horizontally
+
+
+a1 = np.array([1, 2, 3])
+a2 = np.array([4, 5, 6])
+a = np.stack((a1, a2), axis=1) #first it made tranverse and then concatenated
+#stack these two arrays vertically
+print(a)
