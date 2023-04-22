a2cf80af1b596bda7879154a9916fc61e46318c5
Harsh Hirani <hiraniharsh7@gmail.com>
2023-04-22 14:31:30 +0530

    sa3 x4

--- lab.py
+++ lab.py
@@ -1,13 +1,5 @@
-# sa3 x3
-import numpy as np
-arr = np.array([1, 2, 3, 4, 5, 6, 7])
-filter_arr = []
-for element in arr:
-  if element % 2 == 0:
-    filter_arr.append(True)
-  else:
-    filter_arr.append(False)
-newarr = arr[filter_arr]
-print(filter_arr)
-print(newarr)
-#same as above if number is even then their index is True
+# sa3 x4
+import matplotlib.pyplot as plt
+import seaborn as sns
+sns.distplot([0,1,2,3])
+plt.show()
