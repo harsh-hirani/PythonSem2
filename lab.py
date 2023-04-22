41344e09fd1bb3737ff52838c1a98091d0ba209f
Harsh Hirani <hiraniharsh7@gmail.com>
2023-04-22 14:19:48 +0530

    sa3 x2

--- lab.py
+++ lab.py
@@ -1,19 +1,7 @@
-# sa3 x1
+# sa3 x2
 
 import numpy as np
-arr1 = np.array([1, 2, 3])
-arr2 = np.array([4, 5, 6])
-arr = np.concatenate((arr1, arr2))#
-print(arr)
-
-ar1 = np.array([[1, 2], [3, 4]])
-ar2 = np.array([[5, 6], [7, 8]])
-ar = np.concatenate((ar1, ar2), axis=1) 
-print(ar)#concatenate these two arrays horizontally
-
-
-a1 = np.array([1, 2, 3])
-a2 = np.array([4, 5, 6])
-a = np.stack((a1, a2), axis=1) #first it made tranverse and then concatenated
-#stack these two arrays vertically
-print(a)
+arr = np.array([41, 42, 43, 44])
+x = [True, False, True, False]
+newarr = arr[x] #if coressponding index is true it will print the value
+print(newarr)
