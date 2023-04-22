a3edd9ca9a9f1965b379ef84964bca55c17ba033
Harsh Hirani <hiraniharsh7@gmail.com>
2023-04-22 14:42:32 +0530

    sa3 x7

--- lab.py
+++ lab.py
@@ -1,8 +1,12 @@
-# sa3 x6
+# sa3 x7
 import matplotlib.pyplot as plt
 import numpy as np
-xpoints = np.array([0, 6])
-ypoints = np.array([0, 250])
-plt.plot(xpoints, ypoints)
+ypoints = np.array([3, 8, 1, 10])
+plt.plot(ypoints, marker = 'o')
+plt.show()
+plt.plot(ypoints, linestyle = 'dotted')
+plt.show()
+plt.plot(ypoints, linestyle = 'dashed')
 plt.show()
 
+# it will three graph with defined style by making x axix its index and y axes given array
