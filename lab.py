c348dfc4f34ee7f372e1dc183d9371dd0c87d4c8
Harsh Hirani <hiraniharsh7@gmail.com>
2023-04-22 14:32:29 +0530

    sa3 x5

--- lab.py
+++ lab.py
@@ -1,5 +1,8 @@
-# sa3 x4
+# sa3 x5
 import matplotlib.pyplot as plt
-import seaborn as sns
-sns.distplot([0,1,2,3])
+import numpy as np
+xpoints = np.array([0, 6])
+ypoints = np.array([0, 250])
+plt.plot(xpoints, ypoints)
 plt.show()
+
