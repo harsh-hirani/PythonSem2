acff78aebc63ce7567d4fab42c7aeba918fbf65b
Harsh Hirani <hiraniharsh7@gmail.com>
2023-04-23 16:28:36 +0530

    p8 2

--- lab.py
+++ lab.py
@@ -1,19 +1,9 @@
-# p8 1
-from numpy import *
-from pylab import*
-a=linspace(-4,2,6)
-print(a)
-print(a.shape)
-print(type(a))
-b=a**2
-print(b.shape)
-print(b)
-print(type(b))
-plot(a,b)
-show()
-'''
-Explanation :	“a” is a array consisting the values -4 and 2 and other 4 with equal difference.
-And “b” is another array of the square of all elements in “a”
-And by plot it takes x as a points and y as b points 
-And x=y2 funtion like graph is optaind but is has only six points connected by lines on the actual curve.
-'''
+# p8 2
+# Using the given code snippet generate an array x and display the histogram for the generated x.
+
+import numpy as np 
+import matplotlib.pyplot as mlt 
+x =np.random.normal(170,10,250)
+# print(x)\
+mlt.hist(x)
+mlt.show()
