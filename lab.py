505ba9d961575d5326722678249bbf7b9e52415f
Harsh Hirani <hiraniharsh7@gmail.com>
2023-04-23 16:16:49 +0530

    p8 1

--- lab.py
+++ lab.py
@@ -1,10 +1,19 @@
-# sa3 x10
-import matplotlib.pyplot as plt
-import matplotlib.image as im
-
-img = im.imread('88.png')
-plt.imshow(img)
-plt.show()
-
-
-# showing image
+# p8 1
+from numpy import *
+from pylab import*
+a=linspace(-4,2,6)
+print(a)
+print(a.shape)
+print(type(a))
+b=a**2
+print(b.shape)
+print(b)
+print(type(b))
+plot(a,b)
+show()
+'''
+Explanation :	“a” is a array consisting the values -4 and 2 and other 4 with equal difference.
+And “b” is another array of the square of all elements in “a”
+And by plot it takes x as a points and y as b points 
+And x=y2 funtion like graph is optaind but is has only six points connected by lines on the actual curve.
+'''
